2fd3e9389dd4098cbd3c8c21fdfa9b6c5ffa0288
Viper2910 <andrzejrz556@gmail.com>
2020-09-21 12:54:40 +0200

    Creating new array and print element from it

--- nauka.php
+++ nauka.php
@@ -4,6 +4,10 @@
     $myarray = array("Jeden", "Dwa", "Trzy"); //zmienna tablicowa
 
     $username = "Jan Kowalski";
+    echo $username; //wyswietlenie zmiennej $username
 
-    echo $username;
+    echo "<br>"; //Wyswietlenie pustej linii 
+
+    $team = array('Jan', 'Andrzej', 'Kamil', 'Ola', 'Ania'); // Stworzenie tablicy ze zmiennymi typu string 
+    echo $team[3];  //Wyswietlenie 4 elementu tablicy
 ?>
