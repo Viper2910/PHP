185d55c5546f79971b806a716e2b98a040d08ccf
Viper2910 <andrzejrz556@gmail.com>
2020-09-21 12:38:03 +0200

    Adding comments to code

--- nauka.php
+++ nauka.php
@@ -1,5 +1,5 @@
 <?php
-    $mycounter = 1;
-    $mystring = "Hej!";
-    $myarray = array("Jeden", "Dwa", "Trzy");
+    $mycounter = 1; //zmienna numeryczna
+    $mystring = "Hej!"; //zmienna tekstowa
+    $myarray = array("Jeden", "Dwa", "Trzy"); //zmienna tablicowa
 ?>
