ce59f7e18e4b764f9d42f7c4915efa397c3b767d
Viper2910 <andrzejrz556@gmail.com>
2020-09-22 14:37:06 +0200

    Add values to class varriables

--- class.php
+++ class.php
@@ -1,7 +1,14 @@
 <?php
 
 $object = new User; //Stworzenie obiektu klasy User.
-print_r($object); //Wyswietlenie danych.
+print_r($object); echo "<br>";//Wyswietlenie danych.
+
+$object->name = "Janek"; //Przypisanie wartosci dla obiektu.
+$object->password = "haslo123";
+
+print_r($object); echo "<br>"; //Wyswietlenie danych.
+
+$object->save_user();
 
 class User{ //Deklarowanie klasy.
     public $name, $password;
