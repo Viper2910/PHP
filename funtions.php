cd07cb10255be2b48c3b042e6945115540c3076e
Viper2910 <andrzejrz556@gmail.com>
2020-09-22 09:52:24 +0200

    Create function to change string to have the first letter capitalized

--- funtions.php
+++ funtions.php
@@ -1,5 +1,13 @@
 <?php
 
-#code ...
+echo fix_names ("WILIAM", "tomasz", "domiNIK"); //Wyswietlenie wynikow dziala funkcji fix_names
+
+function fix_names($name_1, $name_2, $name_3){ //Zdefiniowanie funkcji wraz z jej parametrami 
+    $name_1 = ucfirst(strtolower($name_1)); //Zamiana lancucha znakow podanego w parametrze najpierw na male litery a nastepnie zmiana pierwszej litery na wielka.
+    $name_2 = ucfirst(strtolower($name_2));
+    $name_3 = ucfirst(strtolower($name_3));
+
+    return $name_1 . " " . $name_2 . " " . $name_3; //Zwrocenie wartosci zamienionych stringow
+}
 
 ?>
