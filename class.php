a2631d8f2567a7d806b19424ff0b1b2257d68e9a
Viper2910 <andrzejrz556@gmail.com>
2020-09-22 14:46:27 +0200

    Create new Class and clone object of that class

--- class.php
+++ class.php
@@ -10,12 +10,26 @@ print_r($object); echo "<br>"; //Wyswietlenie danych.
 
 $object->save_user();
 
+
+$object_1 = new NextUser;
+$object_1->name = "Warian";
+$object_2 = clone $object_1; //Clonowanie obiektu klasy NextUser.
+$object_2->name = "Anduin";
+
+echo "Object 1 name = " . $object_1->name . "<br>";
+echo "Object 2 name = " . $object_2->name . "<br>";
+
+
 class User{ //Deklarowanie klasy.
     public $name, $password;
     
     function save_user(){ //Deklarowanie metody klasy.
-        echo "Miejsce na kod funkcji";
+        echo "Miejsce na kod funkcji <br>";
     }
+}
+
+Class NextUser{ //Deklarowanie klasy 
+    public $name;
 }
 
 ?>
