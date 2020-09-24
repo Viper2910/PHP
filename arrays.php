6e853ef264a0c775fd880c7a765e3fb1dafb7c82
Viper2910 <andrzejrz556@gmail.com>
2020-09-24 17:18:44 +0200

    Create associative array and print its element.

--- arrays.php
+++ arrays.php
@@ -4,4 +4,11 @@ $p1 = array("Ksero", "Atrament", "Laser", "Foto"); //Stworzenie prostej listy.
 
 echo "element p1: " . $p1[2] . "<br>"; //Wyswietlenie trzeceigo elementu listy.
 
+$p2 = array('copier' => "Do kserokopiarek",    //Stworzenie tablicy asocjacyjnej.
+            'inker' => "Do drukarek atramentowych",
+            'laser' => "Do drukarek laserowych",
+            'photo' => "Papier fotograficzny");
+
+echo "element p2: " . $p2['inker'] . "<br>"; //Wyswietlenie elementu tablicy asocjacyjnej.
+
 ?>
